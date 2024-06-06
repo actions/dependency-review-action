bbed6f340aa805712b64b05418048d552ffabcfb
Eli Reisman <elireisman@github.com>
2024-06-05 22:57:06 -0700

    update licenses pkg and tests

--- types/spdx-license-satisfies.d.ts
+++ types/spdx-license-satisfies.d.ts
@@ -3,4 +3,16 @@ declare module '@onebeyond/spdx-license-satisfies' {
     candidateExpr: string,
     constraintExpr: string
   ): boolean
+
+  export function satisfiesAny(
+    candidateExpr: string,
+    licenses: string[]
+  ): boolean
+
+  export function satisfiesAll(
+    candidateExpr: string,
+    licenses: string[]
+  ): boolean
+
+  export function isValid(candidateExpr: string): boolean
 }
