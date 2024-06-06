dfbe08e4d28b017be58b5ab23196a7179b773397
Eli Reisman <elireisman@github.com>
2024-06-05 22:57:06 -0700

    update licenses pkg and tests

--- src/spdx.ts
+++ src/spdx.ts
@@ -8,6 +8,20 @@ export function satisfies(
   return spdx.satisfies(candidateExpr, constraintExpr)
 }
 
+export function satisfiesAny(
+  candidateExpr: string,
+  licenses: string[]
+): boolean {
+  return spdx.satisfiesAny(candidateExpr, licenses)
+}
+
+export function satisfiesAll(
+  candidateExpr: string,
+  licenses: string[]
+): boolean {
+  return spdx.satisfiesAll(candidateExpr, licenses)
+}
+
 // can be a single license or an SPDX expression
 export function isValid(spdxExpr: string): boolean {
   try {
