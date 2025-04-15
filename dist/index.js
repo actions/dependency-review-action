4e5a76e41bb92f4c7f1bff03a5fb728fad074fa4
Josh Soref <2119212+jsoref@users.noreply.github.com>
2025-04-15 08:08:06 -0400

    spelling: vuln
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- dist/index.js
+++ dist/index.js
@@ -748,11 +748,11 @@ function run() {
 function printVulnerabilitiesBlock(addedChanges, minSeverity, warnOnly) {
     return __awaiter(this, void 0, void 0, function* () {
         return core.group('Vulnerabilities', () => __awaiter(this, void 0, void 0, function* () {
-            let vulFound = false;
+            let vulnFound = false;
             for (const change of addedChanges) {
-                vulFound || (vulFound = printChangeVulnerabilities(change));
+                vulnFound || (vulnFound = printChangeVulnerabilities(change));
             }
-            if (vulFound) {
+            if (vulnFound) {
                 const msg = 'Dependency review detected vulnerable packages.';
                 if (warnOnly) {
                     core.warning(msg);
@@ -764,7 +764,7 @@ function printVulnerabilitiesBlock(addedChanges, minSeverity, warnOnly) {
             else {
                 core.info(`Dependency review did not detect any vulnerable packages with severity level "${minSeverity}" or higher.`);
             }
-            return vulFound;
+            return vulnFound;
         }));
     });
 }
